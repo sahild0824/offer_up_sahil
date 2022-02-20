b2e7b797c6ff3fa0aa527e2c2788e3b09966049b
anonymous anonymous <sdesai824@gmail.com>
2022-02-20 16:14:14 +0000

    Whitelist columns in UserMessaging for ActiveAdmin

--- app/admin/user_messagings.rb
+++ app/admin/user_messagings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserMessaging do
 
+ permit_params :recipient_id, :sender_id, :message_data, :product_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
